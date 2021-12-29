32fac815ed612181ba286261de4a6b8632875ec2
mukulchn <mukul_chn@hotmail.com>
2021-12-29 14:33:45 +0000

    initial commit...react

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import Table from './Table'
+import Form from './Form'
+
+class App extends Component {
+
+  state = {
+    characters: [
+      {
+        name: 'Charliez',
+        job: 'Janitor'
+      },
+      {
+        name: 'Macz',
+        job: 'Bouncer'
+      },
+      {
+        name: 'Rider',
+        job: 'Cop'
+      },
+    ],
+  }
+
+    removeCharacter = (index) => {
+      const {characters} = this.state
+
+      this.setState({
+        characters: characters.filter((characters, i) => {
+          return i !== index
+        })
+      })
+    }
+
+    handleSubmit = (character) => {
+      this.setState({characters: [...this.state.characters, character]})
+    }
+  
+    render(){
+      const {characters} = this.state
+      
+      return(
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form handleSubmit={this.handleSubmit} />
+        </div>
+        
+        // <div className="App">
+        //   <h1>Hello, React!!!</h1>
+        // </div>
+      )
+    }
+  }
+
+  export default App
